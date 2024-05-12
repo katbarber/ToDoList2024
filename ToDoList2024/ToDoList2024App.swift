4bc16e3b80d15a711eed0792a71651a7c1109cd4
Kat <kbarber876@gmail.com>
2024-05-11 19:54:18 -1000

    Initial Commit

---
+++ ToDoList2024/ToDoList2024App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList2024App.swift
+//  ToDoList2024
+//
+//  Created by Kat Barber on 5/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
